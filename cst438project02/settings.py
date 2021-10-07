91ed68b4e0b62606fc6ce009fb7696c6810d7f29
keyoni <56132773+keyoni@users.noreply.github.com>
2021-10-06 18:53:50 -0700

    Update settings.py

--- cst438project02/settings.py
+++ cst438project02/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-hipbjc#((lnc5fn8ipg$psp_1#1i@da=hj1*hd3+2=0b)w%54v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wishlistgroupn.herokuapp.com']
 
 
 # Application definition
