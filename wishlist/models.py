9ff4035c7355116daa5b0b35fc881f2d86f4682a
keyoni <56132773+keyoni@users.noreply.github.com>
2021-10-13 00:24:46 -0700

    db working (no docker)

--- wishlist/models.py
+++ wishlist/models.py
@@ -7,21 +7,35 @@ class Users(models.Model):
     first_name = models.CharField(max_length=200)
     user_name = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    
+    class Meta:
+        managed = True
+        db_table = 'User'
+    
     
 class Items(models.Model):
      item_id = models.AutoField(primary_key=True)
-     user_id = models.IntegerField
-     list_id = models.IntegerField
+     user_id = models.IntegerField()
+     list_id = models.IntegerField()
      name = models.CharField(max_length=200)
-     url = models.URLField
-     description = models.TextField
-     picture_url = models.URLField
-     user_priority = models.CharField
+     url = models.URLField()
+     description = models.TextField()
+     picture_url = models.URLField()
+     user_priority = models.CharField(max_length=1000)
      slug = models.SlugField(unique=True)
 
+     class Meta:
+        managed = True
+        db_table = 'Item'
+    
+
 class List(models.Model):
-    listId = models.AutoField(primary_key=True)
-    userId = models.IntegerField
+    list_id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField()
     list_name =  models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        managed = True
+        db_table = 'List'
     
